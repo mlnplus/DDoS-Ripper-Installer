119ea55f99cbb81608d813a21cbb6071df014496
mlnplus <juh4szm1l4n@gmail.com>
2025-11-09 15:31:50 +0100

    Refactor batch script to add folder to PATH

--- drippertopath.bat
+++ drippertopath.bat
@@ -3,15 +3,12 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 set "folderToAdd=C:\Program Files\DDoS-Ripper"
 
-REM Read current user PATH
 for /f "tokens=2,*" %%A in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "xPath=%%B"
 
-REM Check if folder is already in PATH (case-insensitive)
 echo !xPath! | findstr /i /c:"%folderToAdd%" >nul
 if %errorlevel% EQU 0 (
     echo Folder is already in PATH.
 ) else (
-    REM Append folder to user PATH
     setx Path "!xPath!;%folderToAdd%"
     echo Folder added to PATH. Please restart your terminal sessions.
 )
